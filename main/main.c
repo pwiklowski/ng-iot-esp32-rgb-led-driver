5b82bd7bf9ddceb2545c0cb47607cbe65d49081d
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-11-18 20:08:57 +0100

    Add led driver

--- main/main.c
+++ main/main.c
@@ -7,34 +7,78 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#include "esp_spi_flash.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
 
 
-void app_main(void)
-{
-    printf("Hello world!\n");
+#define LEDC_HS_CH0_GPIO (33)
+#define LEDC_HS_CH0_CHANNEL LEDC_CHANNEL_0
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+#define LEDC_TEST_FADE_TIME (3000)
+#define LEDC_TEST_DUTY (4000)
 
-    printf("silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+void app_main(void) {
+  printf("Hello world!\n");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+  int ch;
+
+  /*
+   * Prepare and set configuration of timers
+   * that will be used by LED Controller
+   */
+  ledc_timer_config_t ledc_timer = {
+      .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+      .freq_hz = 5000,                      // frequency of PWM signal
+      .speed_mode = LEDC_HIGH_SPEED_MODE,   // timer mode
+      .timer_num = LEDC_TIMER_0,            // timer index
+      .clk_cfg = LEDC_AUTO_CLK,             // Auto select the source clock
+  };
+  ledc_timer_config(&ledc_timer);
+
+  /*
+   * Prepare individual configuration
+   * for each channel of LED Controller
+   * by selecting:
+   * - controller's channel number
+   * - output duty cycle, set initially to 0
+   * - GPIO number where LED is connected to
+   * - speed mode, either high or low
+   * - timer servicing selected channel
+   *   Note: if different channels use one timer,
+   *         then frequency and bit_num of these channels
+   *         will be the same
+   */
+
+  ledc_channel_config_t ledc_channel = {.channel = LEDC_HS_CH0_CHANNEL,
+                                        .duty = 0,
+                                        .gpio_num = LEDC_HS_CH0_GPIO,
+                                        .speed_mode = LEDC_HIGH_SPEED_MODE,
+                                        .hpoint = 0,
+                                        .timer_sel = LEDC_TIMER_0};
+
+  ledc_channel_config(&ledc_channel);
+
+  // Initialize fade service.
+  ledc_fade_func_install(0);
+
+  while (1) {
+    ledc_set_fade_with_time(ledc_channel.speed_mode, ledc_channel.channel,
+                            LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
+    ledc_fade_start(ledc_channel.speed_mode, ledc_channel.channel,
+                    LEDC_FADE_NO_WAIT);
+    vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
+
+    ledc_set_fade_with_time(ledc_channel.speed_mode, ledc_channel.channel, 0,
+                            LEDC_TEST_FADE_TIME);
+    ledc_fade_start(ledc_channel.speed_mode, ledc_channel.channel,
+                    LEDC_FADE_NO_WAIT);
+    vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
+  }
 }
